56d7e081b89729398fca976c381b537acc212da7
Tomáš Hustoles <tomas.hustoles@gmail.com>
2026-02-23 10:42:26 +0100

    Initial Commit

---
+++ Bug/BugApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugApp.swift
+//  Bug
+//
+//  Created by Tomas Hustoles on 23/2/26.
+//
+
+import SwiftUI
+
+@main
+struct BugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bug
+//
+//  Created by Tomas Hustoles on 23/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
